11309f8d444e11f6ee5e452dae92e45e0015609c
alican <alicanyilmaz101@gmail.com>
2021-07-27 23:29:38 +0300

    notification repository passed to null safety

--- lib/repositories/notification/base_notification_repository.dart
+++ lib/repositories/notification/base_notification_repository.dart
@@ -2,5 +2,5 @@
 import 'package:flutter_sample/models/models.dart';
 
 abstract class BaseNotificationRepository{
-  Stream<List<Future<Notif>>> getUserNotifications({String userId});
+  Stream<List<Future<Notif?>>> getUserNotifications({required String userId});
 }
