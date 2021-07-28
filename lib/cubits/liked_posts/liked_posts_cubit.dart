5860b36b587e7c8604063c7cca1469473c9fa95b
alican <alicanyilmaz101@gmail.com>
2021-07-28 23:07:09 +0300

    liked post cubit passed to null safety

--- lib/cubits/liked_posts/liked_posts_cubit.dart
+++ lib/cubits/liked_posts/liked_posts_cubit.dart
@@ -25,15 +25,15 @@ class LikedPostsCubit extends Cubit<LikedPostsState> {
   void likePost({required Post post}) {
     _postRepository.createLike(post: post, userId: _authBloc.state.user!.uid);
     emit(state.copyWith(
-      likedPostIds: Set<String>.from(state.likedPostIds)..add(post.id),
+      likedPostIds: Set<String>.from(state.likedPostIds)..add(post.id!),
       recentlyLikedPostIds: Set<String>.from(state.recentlyLikedPostIds)
-        ..add(post.id),
+        ..add(post.id!),
     ));
   }
 
   void unlikePost({required Post post}) {
     _postRepository.deleteLike(
-        postId: post.id, userId: _authBloc.state.user!.uid);
+        postId: post.id!, userId: _authBloc.state.user!.uid);
     emit(state.copyWith(
       likedPostIds: Set<String>.from(state.likedPostIds)..remove(post.id),
       recentlyLikedPostIds: Set<String>.from(state.recentlyLikedPostIds)
